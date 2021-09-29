aa9157d18811110420ee455b515ff2c4a98745a5
Marcin Maćkowiak <xspinifex@gmail.com>
2021-09-29 20:47:20 +0200

    change layout.php to bootstrap

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -16,63 +16,58 @@ use App\View;
 abstract class AbstractController
 {
 
-    protected const DEFAULT_ACTION = 'home';
+  protected const DEFAULT_ACTION = 'home';
 
-    private static array $configuration = [];
-  
-    protected ContactModel $contactModel;
-    protected WasteReceiveModel $wasteReceiveModel;
-    protected Request $request;
-    protected View $view;
-  
-    public static function initConfiguration(array $configuration): void
-    {
-      self::$configuration = $configuration;
+  private static array $configuration = [];
+
+  protected ContactModel $contactModel;
+  protected WasteReceiveModel $wasteReceiveModel;
+  protected Request $request;
+  protected View $view;
+
+  public static function initConfiguration(array $configuration): void
+  {
+    self::$configuration = $configuration;
+  }
+
+  public function __construct(Request $request)
+  {
+    if (empty(self::$configuration['db'])) {
+      throw new ConfigurationException('Configuration error');
     }
-  
-    public function __construct(Request $request)
-    {
-      if (empty(self::$configuration['db'])) {
-        throw new ConfigurationException('Configuration error');
-      }
-      $this->contactModel = new ContactModel(self::$configuration['db']);
-      $this->wasteReceiveModel = new WasteReceiveModel(self::$configuration['db']);
-  
-      $this->request = $request;
-      $this->view = new View();
-    }
+    $this->contactModel = new ContactModel(self::$configuration['db']);
+    $this->wasteReceiveModel = new WasteReceiveModel(self::$configuration['db']);
+    $this->request = $request;
+    $this->view = new View();
+  }
 
-    public function chooseModul():void{
-
-
-    }
+  public function chooseModul(): void
+  {
+  }
 
 
   public function run(): void
   {
-   
-    try{
+
+    try {
       $action = $this->action() . 'Action';
-      
-      if (method_exists($this, $action))
-      {
-      $this->$action(); // odpala metode pod nazwą action
+
+      if (method_exists($this, $action)) {
+        $this->$action(); // odpala metode pod nazwą action
       } else {
-       
+
         $action = self::DEFAULT_ACTION . 'Action';
       }
-
-    }catch(StorageException $e){
-      $this -> view-> render(
+    } catch (StorageException $e) {
+      $this->view->render(
         'error',
-        ['message' => $e -> getMessage()]
+        ['message' => $e->getMessage()]
       );
-
-    }catch(NotFoundException $e){
-      $this->redirect('/', ['error' => 'contactNotFound']);     
+    } catch (NotFoundException $e) {
+      $this->redirect('/', ['error' => 'contactNotFound']);
     }
-    
-   // $this->$action();
+
+    // $this->$action();
 
     // switch ($this->action()) {
     //   case 'create':
@@ -83,25 +78,25 @@ abstract class AbstractController
     //     break;
     //   default:
     //   $this -> list();
-        
+
     //     break;
     // }
 
-    
+
   }
 
-  protected function redirect(string $to, array $params):void
+  protected function redirect(string $to, array $params): void
   {
     $location = $to;
     if (count($params)) {
       $queryParams = [];
-      foreach($params as $key => $value){
+      foreach ($params as $key => $value) {
         $queryParams[] = urlencode($key) . '=' . urlencode($value);
       }
-      $queryParams = implode('&',$queryParams);
+      $queryParams = implode('&', $queryParams);
       $location .= $queryParams;
     }
-      
+
     header("Location: $location");
     exit;
   }
@@ -109,10 +104,10 @@ abstract class AbstractController
 
   private function action(): string
   {
-   //  $action = $this -> request->getParam('action');
+    //  $action = $this -> request->getParam('action');
     //var_dump($action);
-   // $data = $this->getRequestGet();
-    return $this -> request->getParam('action', self::DEFAULT_ACTION);
+    // $data = $this->getRequestGet();
+    return $this->request->getParam('action', self::DEFAULT_ACTION);
   }
 
   // private function getRequestGet(): array
--- templates/layout.php
+++ templates/layout.php
@@ -18,30 +18,29 @@
     </div>
   </header>
 
-  <div class="wrapper">
-    <div class="container">
-      <div class="menu">
-        <ul>
-          <li><a href="/">Home Page</a></li>
-          <li><a href="/?module=contacts&action=contactsList">Kontakty</a></li>
-          <li><a href="/?module=contacts&action=createContact">Nowy Kontakt</a></li> <!--   -->
-          <li><a href="/?module=waste&action=wasteReceiveList">Przyjęcia</a></li>
-          <li><a href="/?module=waste&action=wasteReceiveCreate">Nowe Przyjęcie</a></li>
-        </ul>
-      </div>
+  <main class="container-fluid row">
 
-      <div class="page">
+    <nav class="col-4 col-lg-2 mt-4 flex-column">
+      <a class="nav-link bg-light text-info border border-light" href="/">Home</a>
+      <a class="nav-link bg-light text-info border border-light" href="/?module=contacts&action=contactsList">Kontakty</a>
+      <a class="nav-link bg-light text-info border border-light" href="/?module=contacts&action=createContact">Nowy Kontakt</a>
+      <a class="nav-link bg-light text-info border border-light" href="/?module=waste&action=wasteReceiveList">Przyjęcia</a>
+      <a class="nav-link bg-light text-info border border-light" href="/?module=waste&action=wasteReceiveCreate">Nowe Przyjęcie</a>
+    </nav>
 
-        <?php require_once("templates/pages/$page.php");
+    <section class="col-8 col-lg-10">
+      <?php require_once("templates/pages/$page.php");
+      ?>
+    </section>
 
-        ?>
-      </div>
+  </main>
+
+  <footer class="">
+    <div class="container-fluid p-4 bg-cadetblue mb-2 text-center">
+      <p class="text-black-50">Przyjmowanie odpadów - projekt w PHP & bootstrap</p>
     </div>
 
-    <div class="footer">
-      <p>Przyjmowanie odpadów - projekt w PHP</p>
-    </div>
-  </div>
+  </footer>
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
